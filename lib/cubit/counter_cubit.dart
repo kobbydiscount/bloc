1ae3917c6cbf51025432aad28cb6a5e5f2cd99e9
kobbydiscount <32777408+kobbydiscount@users.noreply.github.com>
2020-12-12 13:38:15 +0000

    Learning Bloc Package

---
+++ lib/cubit/counter_cubit.dart
@@ -0,0 +1,14 @@
+import 'package:bloc/bloc.dart';
+import 'package:meta/meta.dart';
+
+part 'counter_state.dart';
+
+class CounterCubit extends Cubit<CounterState> {
+  CounterCubit() : super(CounterState(counter: 0));
+
+  void increment() => emit(CounterState(counter: state.counter + 1));
+
+  void decrement() => emit(CounterState(counter: state.counter - 1));
+
+  void reset() => emit(CounterState(counter: 0));
+}
---
+++ lib/homescreen.dart
@@ -0,0 +1,97 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:learning_bloc/cubit/counter_cubit.dart';
+
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Color(0XFF03C4E5),
+        title: Text("Counter App"),
+        centerTitle: true,
+      ),
+      body: Padding(
+        padding: EdgeInsets.only(left: 15, right: 15, top: 30),
+        child: Container(
+              height: 100,
+              width: MediaQuery.of(context).size.width,
+              color: Colors.red,
+              child: Padding(
+                padding: EdgeInsets.only(left: 20, right: 20),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Container(
+                      height: 60,
+                      width: 60,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(30),
+                        color: Colors.white,
+                      ),
+                      child: GestureDetector(
+                        onTap: (){
+                          print("Increase");
+                          BlocProvider.of<CounterCubit>(context).increment();
+                        },
+                        child: Icon(Icons.add_circle,
+                            color: Color(0XFF03C4E5),
+                            size: 30),
+                      ),
+                    ),
+                    BlocBuilder<CounterCubit, CounterState>(
+                      builder: (BuildContext context, CounterState counterState) {
+                        return Text(
+                          counterState.counter.toString(),
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 40,
+                            fontWeight: FontWeight.bold,
+                          ),);
+                      },
+                    ),
+                    Container(
+                      height: 60,
+                      width: 60,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(30),
+                        color: Colors.white,
+                      ),
+                      child: GestureDetector(
+                        onTap: (){
+                          print("Decrease");
+                          if(BlocProvider.of<CounterCubit>(context).state.counter > 0){
+                            BlocProvider.of<CounterCubit>(context).decrement();
+                          }
+                        },
+                        child: Icon(Icons.remove_circle,
+                          color: Color(0XFF03C4E5),
+                          size: 30,),
+                      ),
+                    ),
+
+                    GestureDetector(
+                     onTap: (){
+                       BlocProvider.of<CounterCubit>(context).reset();
+                     },
+                      child: Container(
+                        child: Icon(Icons.refresh,
+                        size: 30,
+                        color: Colors.yellow),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ),
+
+      ),
+    );
+  }
+}
